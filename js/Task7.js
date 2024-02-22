ff298e1f3b45b131bfbee41f0b612db2af925e0f
Mona LisaVladyslav <143653897+VladyslavPunko@users.noreply.github.com>
2024-02-22 14:31:21 +0100

    1

--- js/Task7.js
+++ js/Task7.js
@@ -1,8 +1,16 @@
-const tusk7 = "==========================================";
-console.log(tusk7);
-//================= task 1 =================
-//================= task 2 =================
-//================= task 3 =================
-//================= task 4 =================
-//================= task 5 =================
-//================= task 6 =================
+import axios from "axios";
+
+console.log(axios.isCancel("something"));
+
+const btn = document.querySelector(".btn");
+console.log(btn);
+
+function getPhotos(inputSearch, page) {
+  const result = fetch(
+    `https://books-backend.p.goit.global/books/category-list`
+  )
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+}
+
+getPhotos();
